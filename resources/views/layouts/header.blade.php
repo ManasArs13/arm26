a799886984a7d80edb5e2c643d53c75c8535b3f1
ManasArs13 <manasyan.a.s@ya.ru>
2022-05-07 22:44:53 +0300

    add auth

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,7 +13,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mx-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Главная</a>
+            <a class="nav-link active" aria-current="page" href="{{ url('/home') }}">Главная</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Создать услугу</a>
