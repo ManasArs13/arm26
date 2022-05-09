807ae7149b6355ccbc3f2c0b5b248e404bed1f38
ManasArs13 <manasyan.a.s@ya.ru>
2022-05-09 10:57:07 +0300

    fix category

--- database/migrations/2022_05_08_200154_create_categories_table.php
+++ database/migrations/2022_05_08_200154_create_categories_table.php
@@ -17,6 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->bigIncrements('id');
             $table->timestamps();
             $table->string('name');
+            $table->string('img');
             $table->bigInteger('sort_id');
         });
     }
